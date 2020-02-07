9f0cba702e406d6a041e884e7636cdcb99a517db
ycloudme <ycloudme@gmail.com>
2020-02-08 01:19:26 +0900

    Updates

---
+++ precache-manifest.e45949c3f707a2fe2a89b5894399dbc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf672c36ad09e22f2dd9e0ccbca8121b",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "ce1964321089e817ca5f",
+    "url": "/movie_app_2019/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "fe7e2cab0773743a5557",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce1964321089e817ca5f",
+    "url": "/movie_app_2019/static/js/main.8c1f2592.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
